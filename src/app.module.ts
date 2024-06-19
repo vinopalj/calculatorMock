2b127a8f5fe0a334d14af4b3375d7b1cb4588591
Jan Vinopal <vinopal.jan@gmail.com>
2024-06-19 12:06:47 +0200

    chore: Add ApiService and ApiModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+
 
 @Module({
-  imports: [],
+  imports: [ApiModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+
 import { join } from 'path';
+dotenv.config();
 
 
 async function bootstrap() {
   const server = express();
-  const port = process.env.PORT || 6000;
+  const port = process.env.PORT || 5005;
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(server),
