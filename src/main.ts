86d9fd9d5f117ed165db7d681a702a699ad70086
Jan Vinopal <vinopal.jan@gmail.com>
2024-06-19 10:15:09 +0200

    chore: Add .env to .gitignore and update npm dependency to dotenv@16.4.5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { join } from 'path';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const server = express();
+  const port = process.env.PORT || 6000;
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
+  app.enableCors();
+  app.use('/public', express.static(join(__dirname, 'public')));
+
+  await app.init();
+
+  server.listen(port, () => {
+    console.log('Nest application is running on port ' + port);
+  });
 }
+
 bootstrap();
